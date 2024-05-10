e398fac12a4d4934b4882ab6b2a4696356a067cf
Wyatt Ptak <ptakwyatt@gmail.com>
2024-05-10 10:39:17 -0700

    Full CRUD data binding app, no db

---
+++ DataBindingPractice/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using DataBindingPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DataBindingPractice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Customer> Customers { get; } = new ObservableCollection<Customer>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Customers.Add(new Customer() { Name="Wyatt"});
+            this.Customers.Add(new Customer() { Name="Grace"});
+            this.Customers.Add(new Customer() { Name="Walker"});
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string tag = button.Tag.ToString();
+            for (int i = 0; i < Customers.Count; i++)
+            {
+                if (Customers[i].Name == tag)
+                {
+                    Customers.RemoveAt(i);
+                }
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string newCustomer = AddCustomerTextBox.Text;
+            Customers.Add(new Customer() { Name=newCustomer });
+        }
+
+        private void AddCustomerTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key== Windows.System.VirtualKey.Enter)
+            {
+                string newCustomer = AddCustomerTextBox.Text;
+                Customers.Add(new Customer() { Name=newCustomer });
+            }
+
+        }
+    }
+}
---
+++ DataBindingPractice/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingPractice.Models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
